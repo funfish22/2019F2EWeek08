a1e9e15c832f39246896ec000fb8e44d3564e0ba
funfish22 <j26391442@gmail.com>
2019-12-30 13:57:56 +0800

    支援IE 11

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import './reset.css';
 import './index.css';
 import './resources/assets/icon/style.css';
