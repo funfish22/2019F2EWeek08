e508dd09b3f699fe78e85ea068484706915d7901
funfish22 <j26391442@gmail.com>
2020-01-06 17:55:41 +0800

    開始使用redux-saga 資料連結到firebase的database，現在只有讀取功能

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.885eb5e2ce8b9051ad4062ab56971b93.js"
+  "./precache-manifest.946ea01ea86a1d99b709f68a9470b8c5.js"
 );
 
 self.addEventListener('message', (event) => {
